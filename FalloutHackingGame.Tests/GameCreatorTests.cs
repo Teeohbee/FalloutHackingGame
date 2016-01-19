e23989819d80865915e7561e574d0e832588c6e8
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-01-19 16:08:45 +0000

    Added comments to test for own understanding

--- FalloutHackingGame.Tests/GameCreatorTests.cs
+++ FalloutHackingGame.Tests/GameCreatorTests.cs
@@ -20,8 +20,11 @@ namespace FalloutHackingGame.Tests
         {
             _dic = File.ReadAllLines("e:\\enable1.txt").ToList();
 
+            // Using mock framework Moq
             _picker = new Mock<IPickAChoice<int>>();
             _picker.Setup(x => x.Pick(It.IsAny<List<int>>())).Returns(5);
+            // Creates the mock of IPickAChoice and fixes the return of Pick to 5,
+            // if any list of integers is provided as an argument.
 
             _gameCreator = new GameCreator("easy", _dic, _picker.Object);
         }
@@ -29,6 +32,7 @@ namespace FalloutHackingGame.Tests
         [TestCase(4)]
         [TestCase(5)]
         [TestCase(6)]
+        // These tests change the return of the _picker.Pick mock to return all test cases.
         public void GetWords_EasyDifficulty_WordLengthsTheSameAsThePickerPicks(int lengthSelected)
         {
             _picker.Setup(x => x.Pick(It.IsAny<List<int>>())).Returns(lengthSelected);
@@ -39,6 +43,7 @@ namespace FalloutHackingGame.Tests
         }
 
         [Test]
+        // Early test to ensure we couldn't cheat and hardcode words.
         public void GetWords_EasyDifficulty_AllWordSuggestionsAreDifferent()
         {
             var words = _gameCreator.GetWords();
@@ -47,6 +52,8 @@ namespace FalloutHackingGame.Tests
         }
 
         [Test]
+        // Rules specify game must provided between, 5 and 15 words. Currently
+        // hardcoded to always return 5 words so currently useless.
         public void GetWords_EasyDifficulty_GivesBetweenFiveAndFifteenElements()
         {
             var words = _gameCreator.GetWords();
@@ -104,6 +111,5 @@ namespace FalloutHackingGame.Tests
 
             Assert.That(state.CorrectCount, Is.EqualTo(2));
         }
-
     }
 }
